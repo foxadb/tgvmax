518be4d778269e646217471b50124e0034389377
Samy Amraoui <sametzy@gmail.com>
2021-06-20 17:22:24 +0200

    Use version 5 of SNCF API

--- src/api/tgv/service.js
+++ src/api/tgv/service.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 
 function tgvMaxUri(origin, destination, start, end) {
-    return 'https://www.oui.sncf/apim/calendar/train/v4/'
-        + origin + '/'
+    return 'https://www.oui.sncf/apim/calendar/v5/outward/'
+        + origin + '-'
         + destination + '/'
         + start + '/'
         + end + '/'
-        + '12-HAPPY_CARD/2/fr?additionalFields=hours&currency=EUR';
+        + '12-HAPPY_CARD?market=fr&vehicleTypes=TRAIN,BUS_TRAIN&travelClass=2&extendedToLocality=true&additionalFields=hours&currency=EUR&client=VSD';
 }
 
 export const findTrainsHours = (origin, destination, date) => {
     return new Promise((resolve, reject) => {
-        axios.get(tgvMaxUri(origin, destination, date, date))
+        const url = tgvMaxUri(origin, destination, date, date);
+        const headers = {
+            // Spoof user agent to bypass SNCF evil check :)
+            'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:89.0) Gecko/20100101 Firefox/89.0'
+        };
+        axios.get(url, { headers })
             .then(response => {
                 const record = response.data[0];
                 const hours = record.price === 0 ? record.hours : [];
